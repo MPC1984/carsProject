3ca547003cbd9bcd3acb2c69908f85c6a182df3b
MPC1984 <33260841+MPC1984@users.noreply.github.com>
2024-04-14 20:18:18 +0200

    Update CarDetailsComponents.java

--- CarsProjectAlbert/src/CarsProjectDev/CarDetailsComponents.java
+++ CarsProjectAlbert/src/CarsProjectDev/CarDetailsComponents.java
@@ -1,39 +1,30 @@
-package CarsProjectDev;
+package com.example.carsprojectdev;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
 /**
- * This class contais the group of text fields representing the cars information visually
- * inside a panel.
- * @
+ * This class contains the group of text fields representing the cars
+ * information visually inside a panel. @
  *
- * PUBLIC FEATURES:
- * // Constructors
- *    public CarDetailsComponents()
+ * PUBLIC FEATURES: // Constructors public CarDetailsComponents()
  *
- * // Methods
- *    public void clearTextFields()
- *    public void componentHidden(ComponentEvent ev)
- *    public void componentMoved(ComponentEvent ev)
- *    public void componentResized(ComponentEvent ev)
- *    public void componentShown(ComponentEvent ev)
- *    public void displayDetails(Car c)
- *    public JPanel getDetailsPanel()
- *    public String getInfoText()
- *    public String getKmText()
- *    public String getManufacturerText()
- *    public String getModelText()
- *    public String getPriceText()
- *    public String getYearText()
- *    public void setFocusManufacturerTextField()
+ * // Methods public void clearTextFields() public void
+ * componentHidden(ComponentEvent ev) public void componentMoved(ComponentEvent
+ * ev) public void componentResized(ComponentEvent ev) public void
+ * componentShown(ComponentEvent ev) public void displayDetails(Car c) public
+ * JPanel getDetailsPanel() public String getInfoText() public String
+ * getKmText() public String getManufacturerText() public String getModelText()
+ * public String getPriceText() public String getYearText() public void
+ * setFocusManufacturerTextField()
  *
  * COLLABORATORS:
  *
  * @version 1.0, 16 Oct 2004
  * @author Adam Black
  */
-public class CarDetailsComponents extends JPanel implements ComponentListener
-{
+public class CarDetailsComponents extends JPanel implements ComponentListener {
 	private JLabel manufacturerLabel = new JLabel("Manufacturer");
 	private JLabel yearLabel = new JLabel("Year");
 	private JLabel modelLabel = new JLabel("Model");
@@ -47,124 +38,123 @@ public class CarDetailsComponents extends JPanel implements ComponentListener
 	private JTextField kmTextField = new JTextField();
 	private JTextArea infoTextArea = new JTextArea(4, 0);
 
-	private final int divFactor = 27;
+	private static final int DIVFACTOR = 27;
 
 	/**
-	 * set up a new CarDetailComponents object and return a reference to the object which is a kind
-	 * of panel
+	 * set up a new CarDetailComponents object and return a reference to the object
+	 * which is a kind of panel
 	 */
-	public CarDetailsComponents()
-	{
+	public CarDetailsComponents() {
 		Insets currentInsets;
 		GridBagConstraints gridBagConstraints;
 		setLayout(new BorderLayout(0, 20));
 		JPanel compPanel = new JPanel(new GridBagLayout());
 		String currentFont = manufacturerLabel.getFont().getName();
-		currentInsets =  new Insets(0, 10, 0, 30);
+		currentInsets = new Insets(0, 10, 0, 30);
 
 		manufacturerLabel.setFont(new Font(currentFont, Font.BOLD, 12));
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.insets = currentInsets;
-        compPanel.add(manufacturerLabel, gridBagConstraints);
-
-        yearLabel.setFont(new Font(currentFont, Font.BOLD, 12));
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.insets = currentInsets;
-        compPanel.add(yearLabel, gridBagConstraints);
-
-        modelLabel.setFont(new Font(currentFont, Font.BOLD, 12));
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.insets = currentInsets;
-        compPanel.add(modelLabel, gridBagConstraints);
-
-        priceLabel.setFont(new Font(currentFont, Font.BOLD, 12));
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 3;
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.insets = currentInsets;
-        compPanel.add(priceLabel, gridBagConstraints);
-
-        kmLabel.setFont(new Font(currentFont, Font.BOLD, 12));
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 4;
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.insets = currentInsets;
-        compPanel.add(kmLabel, gridBagConstraints);
-
-        infoLabel.setFont(new Font(currentFont, Font.BOLD, 12));
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 5;
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.insets = currentInsets;
-        compPanel.add(infoLabel, gridBagConstraints);
-
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.weightx = 1.0;
-        compPanel.add(manufacturerTextField, gridBagConstraints);
+		gridBagConstraints.insets = currentInsets;
+		compPanel.add(manufacturerLabel, gridBagConstraints);
 
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		yearLabel.setFont(new Font(currentFont, Font.BOLD, 12));
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 1;
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.weightx = 1.0;
-        compPanel.add(yearTextField, gridBagConstraints);
+		gridBagConstraints.insets = currentInsets;
+		compPanel.add(yearLabel, gridBagConstraints);
 
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		modelLabel.setFont(new Font(currentFont, Font.BOLD, 12));
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 2;
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.weightx = 1.0;
-        compPanel.add(modelTextField, gridBagConstraints);
+		gridBagConstraints.insets = currentInsets;
+		compPanel.add(modelLabel, gridBagConstraints);
 
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 3;
-        gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		priceLabel.setFont(new Font(currentFont, Font.BOLD, 12));
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 3;
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.weightx = 1.0;
-        compPanel.add(priceTextField, gridBagConstraints);
+		gridBagConstraints.insets = currentInsets;
+		compPanel.add(priceLabel, gridBagConstraints);
 
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 4;
-        gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		kmLabel.setFont(new Font(currentFont, Font.BOLD, 12));
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 4;
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.insets = currentInsets;
+		compPanel.add(kmLabel, gridBagConstraints);
+
+		infoLabel.setFont(new Font(currentFont, Font.BOLD, 12));
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 5;
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.insets = currentInsets;
+		compPanel.add(infoLabel, gridBagConstraints);
+
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.weightx = 1.0;
+		compPanel.add(manufacturerTextField, gridBagConstraints);
+
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 1;
+		gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.weightx = 1.0;
+		compPanel.add(yearTextField, gridBagConstraints);
+
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 2;
+		gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.weightx = 1.0;
+		compPanel.add(modelTextField, gridBagConstraints);
+
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 3;
+		gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.weightx = 1.0;
+		compPanel.add(priceTextField, gridBagConstraints);
+
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 4;
+		gridBagConstraints.gridwidth = GridBagConstraints.RELATIVE;
 		gridBagConstraints.anchor = gridBagConstraints.WEST;
-        gridBagConstraints.weightx = 1.0;
-        compPanel.add(kmTextField, gridBagConstraints);
+		gridBagConstraints.weightx = 1.0;
+		compPanel.add(kmTextField, gridBagConstraints);
 
 		infoTextArea.setLineWrap(true);
 		currentInsets = new Insets(2, 20, 0, 20);
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 5;
-        gridBagConstraints.anchor = gridBagConstraints.WEST;
+		gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 5;
+		gridBagConstraints.anchor = gridBagConstraints.WEST;
 		gridBagConstraints.weightx = 1.0;
-        compPanel.add(new JScrollPane(infoTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), gridBagConstraints);
+		compPanel.add(new JScrollPane(infoTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), gridBagConstraints);
 
 		// this listens for resize events
 		addComponentListener(this);
-        add(compPanel, "North");
+		add(compPanel, "North");
 	}
 
 	/**
 	 * clear all the text fields
 	 */
-	public void clearTextFields()
-	{
+	public void clearTextFields() {
 		manufacturerTextField.setText("");
 		yearTextField.setText("");
 		modelTextField.setText("");
@@ -173,44 +163,45 @@ public class CarDetailsComponents extends JPanel implements ComponentListener
 		infoTextArea.setText("");
 	}
 
-	public void componentHidden(ComponentEvent ev) {}
+	public void componentHidden(ComponentEvent ev) {
+	}
 
-	public void componentMoved(ComponentEvent ev) {}
+	public void componentMoved(ComponentEvent ev) {
+	}
 
 	/**
 	 * when the details panel is resized change the length of the text boxes
 	 *
 	 * @return ev ComponentEvent object
 	 */
-	public void componentResized(ComponentEvent ev)
-	{
-		if (ev.getSource() == this)
-		{
+	public void componentResized(ComponentEvent ev) {
+		if (ev.getSource() == this) {
 			int width = getWidth();
 
-			if (width >= 0)
-			{
-				/** these text fields had to be resized manually. Using insets didn't work for
-				smaller areas of the panel. */
-				manufacturerTextField.setColumns(width / divFactor);
-				yearTextField.setColumns(width / divFactor);
-				modelTextField.setColumns(width / divFactor);
-				priceTextField.setColumns(width / divFactor);
-				kmTextField.setColumns(width / divFactor);
-				infoTextArea.setColumns((width / divFactor) + 3); // this text box is larger
+			if (width >= 0) {
+				/**
+				 * these text fields had to be resized manually. Using insets didn't work for
+				 * smaller areas of the panel.
+				 */
+				manufacturerTextField.setColumns(width / DIVFACTOR);
+				yearTextField.setColumns(width / DIVFACTOR);
+				modelTextField.setColumns(width / DIVFACTOR);
+				priceTextField.setColumns(width / DIVFACTOR);
+				kmTextField.setColumns(width / DIVFACTOR);
+				infoTextArea.setColumns((width / DIVFACTOR) + 3); // this text box is larger
 			}
 		}
 	}
 
-	public void componentShown(ComponentEvent ev){}
+	public void componentShown(ComponentEvent ev) {
+	}
 
 	/**
 	 * display details about a car through the text box components
 	 *
 	 * @param c the car to display details about
 	 */
-	public void displayDetails(Car c)
-	{
+	public void displayDetails(Car c) {
 		manufacturerTextField.setText(c.getManufacturer());
 		yearTextField.setText(Integer.toString(c.getYear()));
 		modelTextField.setText(c.getModel());
@@ -219,41 +210,34 @@ public class CarDetailsComponents extends JPanel implements ComponentListener
 		infoTextArea.setText(c.getInformation());
 	}
 
-	public String getInfoText()
-	{
+	public String getInfoText() {
 		return infoTextArea.getText();
 	}
 
-	public String getKmText()
-	{
+	public String getKmText() {
 		return kmTextField.getText();
 	}
 
-	public String getManufacturerText()
-	{
+	public String getManufacturerText() {
 		return manufacturerTextField.getText();
 	}
 
-	public String getModelText()
-	{
+	public String getModelText() {
 		return modelTextField.getText();
 	}
 
-	public String getPriceText()
-	{
+	public String getPriceText() {
 		return priceTextField.getText();
 	}
 
-	public String getYearText()
-	{
+	public String getYearText() {
 		return yearTextField.getText();
 	}
 
 	/**
 	 * set focus to the manufacturer text field. ie, put the cursor inside it
 	 */
-	public void setFocusManufacturerTextField()
-	{
+	public void setFocusManufacturerTextField() {
 		manufacturerTextField.grabFocus();
 	}
 }
