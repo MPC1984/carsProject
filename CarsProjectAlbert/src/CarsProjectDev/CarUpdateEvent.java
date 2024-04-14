a3e1f54974432d198054e4180a589d2ef94d9cb3
MPC1984 <33260841+MPC1984@users.noreply.github.com>
2024-04-14 20:23:25 +0200

    Update CarUpdateEvent.java

--- CarsProjectAlbert/src/CarsProjectDev/CarUpdateEvent.java
+++ CarsProjectAlbert/src/CarsProjectDev/CarUpdateEvent.java
@@ -1,11 +1,9 @@
-package CarsProjectDev;
+package com.example.carsprojectdev;
+
 /**
- * A basic event that is sent off to registered listeners
- * @
+ * A basic event that is sent off to registered listeners @
  *
- * PUBLIC FEATURES:
- * // Constructors
- *    public CarUpdateEvent(Object source)
+ * PUBLIC FEATURES: // Constructors public CarUpdateEvent(Object source)
  *
  * // Methods
  *
@@ -14,13 +12,11 @@ package CarsProjectDev;
  * @version 1.0, 16 Oct 2004
  * @author Adam Black
  */
-public class CarUpdateEvent extends java.util.EventObject
-{
+public class CarUpdateEvent extends java.util.EventObject {
 	/**
 	 * @param source source object of event
 	 */
-	public CarUpdateEvent(Object source)
-	{
+	public CarUpdateEvent(Object source) {
 		super(source);
 	}
 }
